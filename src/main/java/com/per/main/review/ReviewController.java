0d7b711cdc9389bb5672961527ce1f709d83c345
Yubin-23 <yuyubi151@gmail.com>
2023-08-31 15:35:23 +0900

    0831

--- src/main/java/com/per/main/review/ReviewController.java
+++ src/main/java/com/per/main/review/ReviewController.java
@@ -2,7 +2,10 @@ package com.per.main.review;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import oracle.jdbc.proxy.annotation.Post;
 
 @Controller
 @RequestMapping("/review/*")
@@ -10,6 +13,7 @@ public class ReviewController {
 	@Autowired
 	private ReviewService reviewService;
 	
+	@PostMapping("add")
 	public void setAdd() throws Exception {
 		
 	}
